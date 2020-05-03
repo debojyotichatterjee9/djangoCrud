c4df263844dfab24cb29a950bac29e019d522eca
Debojyoti Chatterjee <debojyoti.chatterjee9@gmail.com>
2020-05-03 23:27:17 +0530

    created CRUD APIs

--- backend/views.py
+++ backend/views.py
@@ -4,6 +4,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import TaskSerializer
 from .models import Task
+from rest_framework import status
 
 # Create your views here.
 
@@ -18,8 +19,48 @@ def apiOverview(request):
     }
     return Response(api_urls)
 
+
+# ============================================ LIST VIEW ============================================ #
 @api_view(['GET'])
 def taskList(request):
     tasks = Task.objects.all()
     serializer = TaskSerializer(tasks, many=True)
     return Response(serializer.data)
+
+
+# ============================================ DETAIL VIEW ============================================ #
+@api_view(['GET'])
+def taskDetail(request, pk):
+    task = Task.objects.get(id=pk)
+    serializer = TaskSerializer(task, many=False)
+    return Response(serializer.data)
+
+
+# ============================================ CREATE VIEW ============================================ #
+@api_view(['POST'])
+def taskCreate(request):
+    serializer = TaskSerializer(data = request.data)
+
+    if serializer.is_valid():
+        serializer.save() # TODO: will have to add the error handling part later
+    return Response(serializer.data)
+
+
+# ============================================ UPDATE VIEW ============================================ #
+@api_view(['POST'])
+def taskUpdate(request, pk):
+    task = Task.objects.get(id=pk)
+    serializer = TaskSerializer(instance = task, data = request.data)
+
+    if serializer.is_valid():
+        serializer.save() # TODO: will have to add the error handling part later
+    return Response(serializer.data)
+
+
+# ============================================ DELETE VIEW ============================================ #
+@api_view(['GET','DELETE'])
+def taskDelete(request, pk):
+    task = Task.objects.get(id=pk)
+    serializer = TaskSerializer(task, many=False)
+    task.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
