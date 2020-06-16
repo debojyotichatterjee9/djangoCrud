ff356a0e820c9b59636ec9571a9e2f02a1c27107
Debojyoti Chatterjee <debojyoti.chatterjee9@gmail.com>
2020-06-16 14:09:53 +0530

    added css and declared the state

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todoList: [],
+      activeItem: {
+        id: null,
+        title: "",
+        completed: false
+      },
+      editing: false
+    }
+  }
+    render() {
+        return (
+            <div className='container'>
+        <div id='task-container'>
+          <div id='form-wrapper'>
+            <form   id="form">
+                    <div className="flex-wrapper">
+                        <div style={{flex: 6}}>
+                            <input  className="form-control" id="title" type="text" name="title" placeholder="Add task.." />
+                         </div>
+
+                         <div style={{flex: 1}}>
+                            <input id="submit" className="btn btn-warning" type="submit" name="Add" />
+                          </div>
+                      </div>
+                </form>
+          </div>
+
+          <div className='list-wrapper'>
+            
+          </div>
+          
+        </div>
+        
+      </div>
+        )
+    }
 }
 
 export default App;
