9889c2364c8d077769d435b449b38543d216ae4b
Debojyoti Chatterjee <debojyoti.chatterjee9@gmail.com>
2020-06-16 16:36:49 +0530

    list view done with css and fetching of data
    added cors-headers in the middleware and whiltelisted frontend url

--- djangoCrud/settings.py
+++ djangoCrud/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     # changes done below this
     'backend.apps.BackendConfig',
     'rest_framework',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -51,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'djangoCrud.urls'
@@ -122,3 +124,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+#  list of origins that are authorized to make cross-site HTTP requests.
+CORS_ORIGIN_WHITELIST = [
+    "http://localhost:3000",
+    "http://192.168.0.129:3000"
+]
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,27 +14,60 @@ class App extends React.Component {
       },
       editing: false
     }
+    this.fetchTasks = this.fetchTasks.bind(this)
+  };
+
+// life-cycle method
+  componentWillMount() {
+    this.fetchTasks();
+  }
+
+// making an API call and rendering out the data
+  fetchTasks() {
+    console.log('fetching...');
+    fetch('http://127.0.0.1:8000/api/task-list/')
+    .then(response => response.json())
+    .then(data => this.setState({
+      todoList: data
+    }));
   }
     render() {
+      let tasks = this.state.todoList;
         return (
             <div className='container'>
         <div id='task-container'>
           <div id='form-wrapper'>
             <form   id="form">
-                    <div className="flex-wrapper">
+                    <div className='flex-wrapper'>
                         <div style={{flex: 6}}>
-                            <input  className="form-control" id="title" type="text" name="title" placeholder="Add task.." />
+                            <input  className='form-control' id='title' type='text' name='title' placeholder='Add task..' />
                          </div>
 
                          <div style={{flex: 1}}>
-                            <input id="submit" className="btn btn-warning" type="submit" name="Add" />
+                            <input id='submit' className='btn btn-warning' type='submit' name='Add' />
                           </div>
                       </div>
                 </form>
           </div>
 
           <div className='list-wrapper'>
-            
+            {
+              tasks.map(function (task, index) {
+                return(
+                    <div key={index} className='task-wrapper flex-wrapper'>
+                      <div style={{flex: 7}}>
+                        <span>{task.title}</span>
+                      </div>
+                      <div style={{flex: 1}}>
+                        <button className='btn btn-sm btn-outline-info'>Edit</button>
+                      </div>
+                      <div style={{flex: 1}}>
+                        <button className='btn btn-sm btn-outline-dark delete'>X</button>
+                      </div>
+                    </div>
+                  );
+              })
+            }
           </div>
           
         </div>
